64a40ea98ac41921688d0655e3614719cdd5f5f8
Chely Ho <adept_inept@yahoo.com>
2018-03-14 22:16:07 +0000

    automatically backed up by learn

---
+++ models/pirate.rb
@@ -0,0 +1,12 @@
+class Pirate
+  attr_accessor :name, :weight, :height
+
+  @@all = []
+
+  def self.all
+    @@all
+  end
+
+  def initialize(params)
+    @name, @weight, @height = params[:name], params[:weight], params[:height]
+end
