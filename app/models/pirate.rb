00d66de1b3c0a5dcc1cb32e5b2ec5267c010f246
Chely Ho <adept_inept@yahoo.com>
2018-03-14 23:04:18 +0000

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,14 @@
 class Pirate
+  attr_accessor :name, :weight, :height
+
+  @@all = []
+
+  def self.all
+    @@all
+  end
+
+  def initialize(params)
+    @name, @weight, @height = params[:name], params[:weight], params[:height]
+    @@all << self
+  end
 end
--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,18 @@
 class Ship
+  attr_accessor :name, :type, :booty
+
+  @@all = []
+
+  def self.all
+    @@all
+  end
+
+  def initialize(params)
+    @name, @type, @booty = params[:name], params[:type], params[:booty]
+    @@all << self
+  end
+
+  def self.clear
+    @@all = []
+  end
 end
