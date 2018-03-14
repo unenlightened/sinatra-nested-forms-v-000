613c8a60dea2f9629707f72b6be1fb6eaed824ee
Chely Ho <adept_inept@yahoo.com>
2018-03-14 22:30:45 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -8,7 +8,7 @@ module FormsLab
     end
 
     get '/new' do
-      erb :new
+      erb pirates/:new
     end
 
     post '/pirates' do
@@ -20,7 +20,7 @@ module FormsLab
 
       @ships = Ship.all
 
-      erb :show
+      erb pirates/:show
     end
 
   end
